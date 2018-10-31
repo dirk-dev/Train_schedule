08111a03902eaf670fd793e04b4768e7fa0e2bdb
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-31 05:39:01 -0400

    updated js and html files

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,30 @@ console.log("train schedule javascript loaded");
 
 $(document).ready(function () {
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCyk2md1GGa3kVqlBN4_e0NDdsxP5JE7x8",
+        authDomain: "train-schedule-54341.firebaseapp.com",
+        databaseURL: "https://train-schedule-54341.firebaseio.com",
+        projectId: "train-schedule-54341",
+        storageBucket: "train-schedule-54341.appspot.com",
+        messagingSenderId: "1057644969419"
+    };
+    firebase.initializeApp(config);
 
-    /* pseudocode
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime = 0;
+    var frequency = 0
+
+   /* pseudocode
 
     * user inputs train name, destination, first train time, frequency in minutes
     use UTC???
-
 
 
     possible code for calculating minutes away:
@@ -19,9 +37,7 @@ $(document).ready(function () {
     current time > var now = moment();
     result % frequency  = minutes away (may need to convert back to human-readable time)
 
-
-    
-
+   
     !!!! need control to limit input to military time, frequency to minutes
 
     * formulas to calculate next arrival time & minutes away
@@ -31,19 +47,23 @@ $(document).ready(function () {
     */
 
 
-
     $("#submit-button").on("click", function () {
 
         event.preventDefault();
 
-        var trainName = $("#train-name").val().trim();
-        var destination = $("#destination").val().trim();
-        var firstTrainTime = $("#first-train-time").val().trim();
-        var frequency = $("#frequency").val().trim();
+        trainName = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrainTime = $("#first-train-time").val().trim();
+        frequency = $("#frequency").val().trim();
 
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
 
-        // firstTrainTime = moment(firstTrainTime);
-        // console.log("The first train moment = " + firstTrainTime);
 
         console.log(trainName, destination, firstTrainTime, frequency);
 
@@ -57,27 +77,13 @@ $(document).ready(function () {
 
         //converts user input into moment data format, specifies format of data input
         firstTrainTime = moment(firstTrainTime, "HH:mma");
-        firstTrainTime = firstTrainTime/1000;
+        firstTrainTime = firstTrainTime / 1000;
         console.log("1st train time= " + firstTrainTime);
 
-        //gets the current time via moment.js
-        //.valueOf returns milliseconds since the Unix Epoch (1/1/1970 00:00:00)
-        var currentTime = moment().unix();
-        // currentTime = currentTime/1000;
-        console.log("var currentTime format = ", currentTime);
-
-        // (currentTime, "HH:mma");
-
-
-        //doesn't work!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        var currentReadableTime = moment(currentTime, "HH:mma");
-        console.log("current time in readable form is: " + currentReadableTime);
-
-
+  
         //TODO:
         //parse time values to display human-readable format
-
-
+        //take into account time zones
 
 
         var timeDifference = (currentTime - firstTrainTime);
@@ -94,21 +100,33 @@ $(document).ready(function () {
 
     });
 
+    //gets the current time via moment.js
+    //.valueOf returns Unix time in seconds since the Unix Epoch (1/1/1970 00:00:00)
+    var currentTime = moment().unix();
+    console.log("var currentTime format = ", currentTime);
+    // console.log(currentTime.format("HH/mm/ss"));
 
 
+    // Firebase watcher .on("child_added"
+    database.ref().on("child_added", function (snapshot) {
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
 
+        // Console.loging the last user's data
+        console.log(sv.trainName);
+        console.log(sv.destination);
+        console.log(sv.firstTrainTime);
+        console.log(sv.frequency);
 
+        // Change the HTML to reflect
+        $("#name-display").text(sv.trainName);
+        $("#email-display").text(sv.destination);
+        $("#age-display").text(sv.firstTrainTime);
+        $("#comment-display").text(sv.frequency);
 
-
-
-
-
-
-
-
-
-
-
-
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
 
 });
