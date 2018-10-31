70a92ed1dfe025789a7ca4675dd60ef0fac4a047
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-31 09:50:23 -0400

    updated JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,59 +4,48 @@ $(document).ready(function () {
 
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyCdDR8d6onMkdtLN-BtK59AAtEVojNAft0",
-        authDomain: "recentuser-44024.firebaseapp.com",
-        databaseURL: "https://recentuser-44024.firebaseio.com",
-        projectId: "recentuser-44024",
-        storageBucket: "recentuser-44024.appspot.com",
-        messagingSenderId: "303535582852"
-      };
+        apiKey: "AIzaSyCyk2md1GGa3kVqlBN4_e0NDdsxP5JE7x8",
+        authDomain: "train-schedule-54341.firebaseapp.com",
+        databaseURL: "https://train-schedule-54341.firebaseio.com",
+        projectId: "train-schedule-54341",
+        storageBucket: "train-schedule-54341.appspot.com",
+        messagingSenderId: "1057644969419"
+    };
     firebase.initializeApp(config);
 
     // Create a variable to reference the database.
     var database = firebase.database();
 
-    // var trainName = "";
-    // var destination = "";
-    // var firstTrainTime = 0;
-    // var frequency = 0
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime;
+    var frequency = "";
+    var nextArrival = "";
+    var minutesAway = "";
 
     /* pseudocode
-
      * user inputs train name, destination, first train time, frequency in minutes
      use UTC???
-
      possible code for calculating minutes away:
-
      convert all times to 'time from epoch'
-
      current time - first train time = result
-
-     current time => var now = moment();
+     current time > var now = moment();
      result % frequency  = minutes away (may need to convert back to human-readable time)
-
    
-     !!!! need control to limit input to military time, frequency to positive minutes
-
+     !!!! need control to limit input to military time, frequency to minutes
      * formulas to calculate next arrival time & minutes away
-
      * on click auto populates on-screen table
      */
 
-    // data not persisting in browser
-    // time is not taking time zone into account
-    // time displayed in next arrival not in hh:mm
-    // formula for minutes away not correct
 
-
-    $("#submit-button").on("click", function (event) {
+    $("#submit-button").on("click", function () {
 
         event.preventDefault();
 
-        var trainName = $("#train-name").val().trim();
-        var destination = $("#destination").val().trim();
-        var firstTrainTime = $("#first-train-time").val().trim();
-        var frequency = $("#frequency").val().trim();
+        trainName = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrainTime = $("#first-train-time").val().trim();
+        frequency = $("#frequency").val().trim();
 
         database.ref().push({
             trainName: trainName,
@@ -66,58 +55,71 @@ $(document).ready(function () {
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
 
-        console.log(trainName, destination, firstTrainTime, frequency);
+
+        console.log("User entered data - train name: " + trainName + " destination: " + destination + "1st train time: " + firstTrainTime + " frequency: " + frequency);
 
         //converts user input into moment data format, specifies format of data input
-        firstTrainTime = moment(firstTrainTime, "HH:mma");
-        firstTrainTime = firstTrainTime / 1000;
-        console.log("1st train time: " + firstTrainTime);
+        firstTrainTime = moment(firstTrainTime, "HH:mm");
+        console.log("1st train time= " + firstTrainTime);
 
-        var timeDifference = (currentTime - firstTrainTime);
-        console.log("TimeDifference = " + timeDifference);
 
-        var minutesAway = Math.abs(((currentTime - firstTrainTime) % frequency));
+        //TODO:
+        //parse time values to display human-readable format
+        //take into account time zones
+
+
+        timeDifference = (currentTime - firstTrainTime);
+        console.log("TimeDifference: " + timeDifference);
+
+        minutesAway = Math.abs(((currentTime - firstTrainTime) % frequency));
         console.log("the next train is: " + minutesAway + " minutes away");
 
-        var nextArrival = currentTime + minutesAway;
+        nextArrival = currentTime + minutesAway;
+        //formats in 
+        nextArrival = (moment(nextArrival).format("HH:mm A"));
         console.log("next arrival is: " + nextArrival);
 
+        //converts unix timestamp to human readable time
+        // var hoursConvert = 
+        // var minutesConvert = 
+
         $("#trainData").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
 
     });
 
     //gets the current time via moment.js
-    var currentTime = moment().unix();
+    //.valueOf returns Unix time in seconds since the Unix Epoch (1/1/1970 00:00:00)
+    var currentTime = moment();
     console.log("var currentTime format = ", currentTime);
-
+    console.log(currentTime.format("HH/mm/ss"));
 
 
     // Firebase watcher .on("child_added"
-    database.ref().on("child_added", function (childSnapshot) {
+    database.ref().on("child_added", function (snapshot) {
         // storing the snapshot.val() in a variable for convenience
-        // var sv = snapshot.val();
+        var sv = snapshot.val();
 
         // Console.loging the last user's data
-        console.log(childSnapshot.val().trainName);
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().firstTrainTime);
-        console.log(childSnapshot.val().frequency);
+        console.log(sv.trainName);
+        console.log(sv.destination);
+        console.log(sv.firstTrainTime);
+        console.log(sv.frequency);
+        console.log(sv.dateAdded);
+
 
         // Change the HTML to reflect
-
-
+        $("#name-display").text(sv.trainName);
+        $("#email-display").text(sv.destination);
+        $("#age-display").text(sv.firstTrainTime);
+        $("#comment-display").text(sv.frequency);
 
         // Handle the errors
-    }, 
-    
-    function (errorObject) {
+    }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
 
-    $("#train-name").text(childSnapshot.val().trainName);
-    $("#destination").text(childSnapshot.val().destination);
-    $("first-train-time").text(childSnapshot.val().firstTrainTime);
-    $("#frequency").text(childSnapshot.val().frequency);
+    $("#trainData").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 
 
 });
